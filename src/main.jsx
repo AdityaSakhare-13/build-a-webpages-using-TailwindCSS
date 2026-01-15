543b9244117f19bf9b67af488d10f9cda3da93eb
AdityaSakhare-13 <sakhareaditya203@gmail.com>
2026-01-15 12:11:22 +0530

    added toml file4

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import{BrowserRouter,Routes,Route} from 'react-router';
+import{HashRouter,Routes,Route} from 'react-router';
 import Home from './views/Home.jsx';
 import AllBicycles from './views/AllBicycles.jsx';
 import Contact from './views/Contact.jsx';
@@ -13,14 +13,14 @@ const rootElement = createRoot(document.getElementById('root'));
 
 rootElement.render(
 <div className='bg-orange-100'>
-    <BrowserRouter>
+    <HashRouter>
         <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/explore" element={<AllBicycles/>}/>
             <Route path="/contact" element={<Contact/>}/>
             <Route path="/Bicycle-details" element={<BicycleDetails/>}/>
         </Routes>
-    </BrowserRouter>
+    </HashRouter>
 </div>
 );
 
